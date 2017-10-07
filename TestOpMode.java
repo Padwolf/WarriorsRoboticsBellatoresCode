9fc8abf5fe55cd620181928a441988bf084fed46
Codewolf <padwolf99@gmail.com>
2017-10-07 13:35:07 -0600

    Working Holonomic Drive

--- TestOpMode.java
+++ TestOpMode.java
@@ -10,6 +10,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name="Test Op")
 public class TestOpMode extends OpMode {
     HardwareHolonomic hardware;
+    private final double maxSpeed = 0.5;
     @Override
     public void init() {
         hardware = new HardwareHolonomic(hardwareMap);
@@ -17,14 +18,27 @@ public class TestOpMode extends OpMode {
 
     @Override
     public void loop() {
-        double[] powers = calcWheelPowers(gamepad1.right_stick_x, gamepad1.right_stick_y);
+        double speed = Math.sqrt(Math.pow(gamepad1.right_stick_x, 2) +
+                Math.pow(gamepad1.right_stick_y, 2));
+        double[] powers = calcWheelPowers(gamepad1.right_stick_x, gamepad1.right_stick_y,
+                gamepad1.left_stick_x, speed);
+        telemetry.addData("W0", powers[0]);
+        telemetry.addData("W1", powers[1]);
+        telemetry.addData("W2", powers[2]);
+        telemetry.addData("W3", powers[3]);
+
+
+        for (int i = 0; i < powers.length; i++) {
+            powers[i] *= maxSpeed;
+        }
+
         hardware.dnw.setPower(powers[0]);
         hardware.dne.setPower(powers[1]);
         hardware.dsw.setPower(powers[2]);
         hardware.dse.setPower(powers[3]);
     }
 
-    static double[] calcWheelPowers (double x, double y) {
+    static double[] calcWheelPowers (double x, double y, double turn, double speed) {
 
 	/*
 	  Holonomic Wheel Layout
@@ -43,8 +57,16 @@ public class TestOpMode extends OpMode {
 
         powers[0] = Math.cos(angle);
         powers[1] = Math.sin(angle);
-        powers[2] = -Math.cos(angle);
-        powers[3] = -Math.sin(angle);
+        powers[2] = -Math.sin(angle);
+        powers[3] = -Math.cos(angle);
+
+        for (int i = 0; i < powers.length; i++) {
+            powers[i] *= speed;
+        }
+
+        for (int i = 0; i < powers.length; i++) {
+            powers[i] = GenFuncs.constrain(powers[i] + (turn * 0.5), 1, -1);
+        }
 
         return powers;
     }
